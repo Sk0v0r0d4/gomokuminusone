6842a281cdf9b72bb9f3afbaa7feea82b2ba3a98
leet_feet <sk0v0r0d4.1@gmail.com>
2025-07-18 19:51:03 +0500

    moved board draw to gfx

--- gfx.lua
+++ gfx.lua
@@ -1,3 +1,47 @@
+function board_draw()
+    map()
+    for i = 1, #board do
+        local line = board[i]
+        for j = 1, #line do
+            if line[j] != 0 then
+                x = (8 * j)
+                y = (8 * i)
+                spr(3 + line[j], x, y)
+            end
+        end
+    end
+    rectfill(
+        0,
+        128,
+        36,
+        120,
+        2
+    )
+    spr(
+        3 + player,
+        0,
+        15 * 8
+    )
+    print(
+        "'s move",
+        8,
+        (15 * 8) + 2,
+        15
+    )
+end
+
+function pointer_draw()
+    x = (8 * pointer[2])
+    y = (8 * pointer[1])
+    spr(36, x, y)
+    --arrow jumping animation
+    spr(
+        20 + (fc / 10 % 2),
+        x,
+        y - 6
+    )
+end
+
 POPUP_DELAY = 30
 current_popup = {
     text = "",
