9ce606c68316815a0f130ec34c4e245aaaac06e7
leet_feet <sk0v0r0d4.1@gmail.com>
2025-07-21 22:33:24 +0500

    BG done

--- bg_gfx.lua
+++ bg_gfx.lua
@@ -3,17 +3,28 @@ gradient = {
 }
 
 function bg_color(x, y)
-    x += fc / 40
-    y -= fc / 60
-    x += cos(fc / 145) * 3
-    y += cos(fc / 99) * 3
-    sin_1 = abs(sin(x / 30) + 0.99)
-    sin_2 = abs(sin(y / 15) + 0.99)
-    index = ceil((sin_1 + sin_2) * 1.2)
+    x = x / 2
+    local angle = fc / 1200
+    -- local angle = 0
+
+    local new_x = x * sin(angle) + y * cos(angle)
+    local new_y = x * cos(angle) - y * sin(angle)
+
+    new_x *= 0.9 + cos(fc / 300) * 0.5
+    new_y *= 0.9 + cos(fc / 300) * 0.5
+    new_x += fc / 40
+    new_y -= fc / 60
+    new_x += cos(fc / 145) * 3
+    new_y += cos(fc / 99) * 3
+
+    local sin_1 = abs(sin(new_x / 16) + 0.99)
+    local sin_2 = abs(sin(new_y / 16) + 0.99)
+    local index = ceil((sin_1 + sin_2) * 1.2)
     return gradient[index]
 end
 
 function bg_draw()
+    fillp(░)
     for i = 0, 15 do
         for j = 0, 15 do
             local x = i * 4
@@ -23,4 +34,6 @@ function bg_draw()
             rectfill(124 - x, y, 124 - x + 3, y + 7, col)
         end
     end
+    fillp()
+    -- print(fc, 1, 1, 7)
 end
